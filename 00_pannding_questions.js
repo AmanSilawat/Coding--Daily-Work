8b1d322ad754798246d23a96afdbadfb45e046a8
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2020-12-30 22:07:46 +0530

    Added 2 pannding questions

---
+++ 00_pannding_questions.js
@@ -0,0 +1,14 @@
+// Q1 ....
+let arr = [
+    ['a', 'd', 'd', 'v', 'd'],
+    ['h', 'h', 'v', 'd', 'f'],
+    ['n', 'i', 'i', 'p', 'o'],
+    ['t', 'i', 'y', 'u', 'i'],
+    ['r', 'e', 'w', 'a', 'u']
+];
+
+isWord();
+isSubstringOfAWord();
+
+
+// Q2. create promise from strach

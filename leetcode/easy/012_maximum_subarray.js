bbf924c9f0cfdd2d2305fc9fa0fbf71cfb7755f7
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-02-24 21:37:45 +0530

    Not solved

--- leetcode/easy/012_maximum_subarray.js
+++ leetcode/easy/012_maximum_subarray.js
@@ -19,33 +19,34 @@ let maxSubArray__ = function (nums) {
             let arr = nums.slice(j, j + len)
             let sum = arr.reduce((acc, val) => acc + val)
             console.log(JSON.stringify(arr), ' = ', sum)
-
         }
     }
 };
 
 
-let maxSubArray = function (nums) {
+let maxSubArray___ = function (nums) {
     if (nums.length == 1) {
         return nums[0]
-    } else if (nums.length == 2) {
-        let posOrNeg = nums[0] < 0 || nums[1] < 0 ? 'neg' : 'pos';
-        let n1 = Math.abs(nums[0]);
-        let n2 = Math.abs(nums[1]);
-        let sum;
-        if (posOrNeg == 'neg') {
-            sum = n1 - n2;
-        } else {
-            sum = n1 + n2;
-        }
-        console.log(sum)
-
-        return nums[0] < 0 && nums[1] < 0 ? Number(`-${Math.abs(sum)}`) : sum;
     }
+    // else if (nums.length == 2) {
+
+    //     let posOrNeg = nums[0] < 0 || nums[1] < 0 ? 'neg' : 'pos';
+    //     let n1 = Math.abs(nums[0]);
+    //     let n2 = Math.abs(nums[1]);
+    //     let sum;
+    //     if (posOrNeg == 'neg') {
+    //         sum = n1 - n2;
+    //     } else {
+    //         sum = n1 + n2;
+    //     }
+    //     console.log(sum)
+
+    //     return nums[0] < 0 && nums[1] < 0 ? Number(`-${Math.abs(sum)}`) : sum;
+    // }
 
     let maxSum = -Infinity;
     console.log(nums.length)
-    for (let len = 2; len < nums.length + 1; len++) {
+    for (let len = 1; len < nums.length + 1; len++) {
         console.log(len)
         for (let j = 0; j < nums.length - len + 1; j++) {
             let arr = nums.slice(j, j + len)
@@ -60,9 +61,34 @@ let maxSubArray = function (nums) {
     return maxSum;
 };
 
+
+let maxSubArray = function (nums) {
+    if (nums.length == 1) {
+        return nums[0]
+    }
+    let max_so_far = max_ending_here = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        max_ending_here = max_ending_here + nums[i]
+
+        if (max_so_far < max_ending_here) {
+            max_so_far = max_ending_here
+        }
+
+        if (max_ending_here < 0) {
+            max_ending_here = 0
+        }
+
+    }
+
+    return max_so_far;
+}
+
+
 // console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
 // console.log(maxSubArray([-2, 1])); // 1
 // console.log(maxSubArray([-2, -1])); // -1
-// console.log(maxSubArray([-1, -2])); // -1
-console.log(maxSubArray([-1, 0])); // 0
-
+// console.log(maxSubArray([5, 10, 10])); // 25
+// console.log(maxSubArray([5, 10])); // 15
+// console.log(maxSubArray([-2, -1])); // -1
+console.log(maxSubArray([-10, -5, -20, -1])); // -1

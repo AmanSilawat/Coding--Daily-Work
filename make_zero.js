1416796ceaca53e84c519aabc061019660d28fd3
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-03-22 16:07:41 +0530

    optimize make zero

--- make_zero.js
+++ make_zero.js
@@ -17,18 +17,27 @@ function make_zero(arr) {
     }
 
     // set zero
-    for (let row = 0; row < row_len; row++) {
-        for (let col = 0; col < col_len; col++) {
-            if (typeof find_pos.row[row] != 'undefined') {
-                arr[row][col] = 0;
-            }
-            if (typeof find_pos.col[col] != 'undefined') {
-                arr[row][col] = 0;
-            }
-        }
-    }
+    // for (let row = 0; row < row_len; row++) {
+    //     for (let col = 0; col < col_len; col++) {
+    //         if (typeof find_pos.row[row] != 'undefined') {
+    //             arr[row][col] = 0;
+    //         }
+    //         if (typeof find_pos.col[col] != 'undefined') {
+    //             arr[row][col] = 0;
+    //         }
+    //     }
+    // }
 
-    return arr
+    // optimize the code
+    let res_arr = Array.from({ length: row_len }, (val, row) => Array.from({ length: col_len }, (val, col) => {
+        if (typeof find_pos.row[row] != 'undefined' || typeof find_pos.col[col] != 'undefined') {
+            return 0;
+        }else {
+            return 1;
+        }
+    }))
+
+    return res_arr;
 }
 
 let arr = [

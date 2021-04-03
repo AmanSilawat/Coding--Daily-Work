4b22e5eabc540fef70082f726e77b37d59362027
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-04-04 01:31:11 +0530

    count_syllables

---
+++ regex.js
@@ -0,0 +1,6 @@
+function count_syllables(str) {
+    return str.length / 2;
+}
+console.log(count_syllables('Hehehehehehe'));
+console.log(count_syllables('bobobobobobobobo'));
+console.log(count_syllables('NANANA'));

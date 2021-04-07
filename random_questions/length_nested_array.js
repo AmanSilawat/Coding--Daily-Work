ef30155d4f3a19325a11e3a3d8dcb2fba248a9e4
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-04-07 20:44:56 +0530

    Length of a Nested Array

---
+++ random_questions/length_nested_array.js
@@ -0,0 +1,10 @@
+// ! Length of a Nested Array
+
+function getLength(array) {
+    return array.flat(Infinity).length;
+}
+
+console.log(getLength([1, [2, 3]])); // 3
+console.log(getLength([1, [2, [3, 4]]])); // 4
+console.log(getLength([1, [2, [3, [4, [5, 6]]]]])); // 6
+console.log(getLength([1, [2], 1, [2], 1])); // 5

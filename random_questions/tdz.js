f8e1a2078345499b75b7d133b7f0e1398834ef0a
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-04-14 23:37:19 +0530

    tdz

---
+++ random_questions/tdz.js
@@ -0,0 +1,13 @@
+// Temporal Dead Zone ?
+
+x = 23; // Gives reference error
+
+let x;
+
+
+function anotherRandomFunc() {
+    message = "Hello"; // Throws a reference error
+
+    let message;
+}
+anotherRandomFunc();

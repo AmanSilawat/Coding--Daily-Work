5b64d86771168c86d4d1686077a42e00c5f4287c
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-05-12 17:28:51 +0530

    synchronous fetch call

---
+++ random_questions/sync-api-call.js
@@ -0,0 +1,30 @@
+let arr = [
+    'https://jsonplaceholder.typicode.com/todos/1',
+    'https://jsonplaceholder.typicode.com/todos/2'
+];
+
+const makeCallInSequence = (apiArray, callback) => {
+    let i = 0,
+        result = [];
+    const onResponse = response => {
+        result.push(response);
+        i++;
+        if (i < apiArray.length) {
+            request(apiArray[i]);
+        } else {
+            callback(result);
+        }
+    }
+
+    const request = api => {
+        fetch(api).then(onResponse).catch(onResponse);
+    }
+
+    request(apiArray[0])
+}
+
+const cb = async res => Promise.all(res)
+    .then(responses => Promise.all(responses.map(response => response.json())))
+    .then(data => console.log(data));
+    
+makeCallInSequence(arr, cb)

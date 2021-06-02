bddd190a35f8ee5c62df05ddb5e1389ddea7ae34
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-06-02 22:43:30 +0530

    Binary Search Tree

---
+++ tree/binary-search-tree/script.js
@@ -0,0 +1,52 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+// Binary Search tree class
+class BinarySearchTree {
+    constructor() {
+        // root of a binary seach tree
+        this.root = null;
+    }
+
+    insert(data) {
+        var new_node = new Node(data);
+
+        if (this.root == null) {
+            this.root = new_node;
+        } else {
+            this.insert_node(this.root, new_node)
+        }
+    }
+
+    insert_node(node, new_node) {
+        if (new_node.data < node.data) {
+            if (node.left == null) {
+                node.left = new_node;
+            } else {
+                this.insert_node(node.left, new_node)
+            }
+        } else {
+            if (node.right == null) {
+                node.right = new_node;
+            } else {
+                this.insert_node(node.right, new_node)
+            }
+        }
+    }
+}
+
+// create an object for the BinarySearchTree
+var BST = new BinarySearchTree();
+
+
+BST.insert(50);
+
+BST.insert(20);
+
+BST.insert(25);
